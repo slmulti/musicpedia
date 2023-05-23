23db7808e4c29c4f3bd24890dab354cf495aa71e
slmutli <108276889+slmulti@users.noreply.github.com>
2023-05-23 17:31:09 +0100

    dont hit refresh

--- src/components/layout/artistinfo/GetAlbumsByArtistID.jsx
+++ src/components/layout/artistinfo/GetAlbumsByArtistID.jsx
@@ -15,15 +15,31 @@ function GetAlbumsByArtistID({ albums }) {
     console.log("Filtered return of Albums: ", filteredAlbums);
 
     return (
-        <div>
-            <h1 className="text-6xl">Albums</h1>
-            <div className="grid grid-cols-1 gap-8 xl:grid-cols-10 lg:grid-cols-5 md:grid-cols-2">
+        <div className="mt-8">
+            <h1 className="text-6xl mb-4">Albums</h1>
+            <div className="grid grid-cols-2 gap-8 xl:grid-cols-6 lg:grid-cols-4 md:grid-cols-3">
                 {filteredAlbums.map((album) => (
-                    <div>
+                    // <div>
+                    //     <figure>
+                    //         <img src={album.images[1].url} alt={album.name} />
+                    //     </figure>
+                    //     <h3>{album.name}</h3>
+                    // </div>
+                    <div className="card card-compact w-48 bg-base-100 shadow-xl hover:scale-105 ">
                         <figure>
                             <img src={album.images[1].url} alt={album.name} />
                         </figure>
-                        <h3>{album.name}</h3>
+                        <div className="card-body">
+                            <h2 className="text-lg text-center">
+                                {album.name}
+                            </h2>
+
+                            {/* <div className="card-actions justify-end">
+                                <button className="btn btn-primary">
+                                    Listen
+                                </button>
+                            </div> */}
+                        </div>
                     </div>
                 ))}
             </div>
